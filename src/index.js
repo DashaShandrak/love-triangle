bc103b50649103b6219a07d0575b9935aa87430e
DashaShandrak <d.shandrak@yandex.by>
2019-03-15 18:32:02 +0300

    add: add completed tack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+* @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var countTriangle = 0;
+  for(var i = 0; i < preferences.length; i++) {
+    var firstNumber = i+1; 
+    var firstLove = preferences[i]; 
+    if(firstLove != firstNumber && !isNaN(firstLove)) {
+      var secondLove = preferences[firstLove-1]; 
+      if(!isNaN(secondLove) && secondLove != firstLove) {
+        if(secondLove != firstNumber) {
+         var thirdlove = preferences[secondLove-1]; 
+         if(!isNaN(thirdlove) && thirdlove != secondLove) {
+           if(thirdlove != firstLove) {
+             if(thirdlove == firstNumber) {
+             countTriangle++;
+             preferences[i] = "*";
+             preferences[firstLove-1] = "*";
+             preferences[secondLove-1] = "*";
+           }
+          }  
+        }else{
+        preferences[thirdlove-1] = "*";
+       }
+      }
+    }
+    else {
+    preferences[secondLove-1] = "*";
+    }
+  }else {
+  preferences[i] = "*";
+}
+}
+return countTriangle; 
 };
+
